b41c7585f0cdf67fe5909f5038dd0bba8cf1a8c7
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-07-03 02:04:32 +0500

    editor added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    let editor: any;
+  
+    onMount(async () => {
+      const EditorJS = await import('@editorjs/editorjs');
+      const Quote = (await import('@editorjs/quote')).default;
+      const List = (await import('@editorjs/list')).default;
+      const Table = (await import('@editorjs/table')).default;
+      const Header = (await import('@editorjs/header')).default;
+  
+  
+      editor = new EditorJS.default({
+        holder: 'editor',
+        placeholder: 'Type something...',
+        tools: {
+          header: Header,
+          list: List,
+          quote: Quote,
+          table: Table,
+        } 
+      });
+    });
+  
+  </script>
+  
+  <div id="editor"></div>
